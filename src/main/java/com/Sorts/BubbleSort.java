91e55db0275b5f77d98c07f8e170f426ded7caea
DiegoLinares11 <lin221256@uvg.edu.gt>
2024-12-09 11:32:13 -0600

    Sorts and notacion big O

---
+++ src/main/java/com/Sorts/BubbleSort.java
@@ -0,0 +1,28 @@
+package com.Sorts ;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        // Recorremos el arreglo
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                // Si el elemento actual es mayor que el siguiente, los intercambiamos
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        
+        System.out.println("Arreglo ordenado:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
